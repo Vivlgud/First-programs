d1eb33010f6695b600bf8c07cab8acb4eada51eb
Vitaly <vivlgud@mail.ru>
2022-09-14 10:26:10 +0200

    Added Exam04 Lesson1 Hi Masha

---
+++ Exam104/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Enter your name ");
+string username=Console.ReadLine();
+
+if(username.ToLower()=="Masha")
+{
+    Console.WriteLine("Hiiii,Masha!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
